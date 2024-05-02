7120162c50e620b294db43ea0a7f0a7c3d73ffc0
abdusamad8575 <samadpts786313@gmail.com>
2024-05-02 11:28:45 +0530

    updation completed

--- js/main.js
+++ js/main.js
@@ -100,3 +100,17 @@
 
 
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzSZ8Yg2IrQWUqhgQHnV_5C0mAxNu5p399-BLObshNYscFOzAT5NQrFXx5-m3qt7K9_TA/exec'
+
+const form = document.forms['contact-form']
+const submitButton = document.querySelector('button[type="submit"]');
+
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ submitButton.disabled = true;
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+ .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(() => { window.location.reload(); })
+ .catch(error => console.error('Error!', error.message))
+})
